950616b3dd1267de46bbfdcd1d351a599f669e30
Admin <Admin@MacBook-Pro.homenet.telecomitalia.it>
2022-07-19 19:13:56 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import ReactGA from 'react-ga';
 import './App.css';
 import InputForm from './components/InputForm';
+
+const TRACKING_ID = "G-G6CNVXNVTF"
+ReactGA.initialize(TRACKING_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
