47cefbad749adefa9cc9956cac286aaa14b3146c
Admin <Admin@MacBook-Pro.homenet.telecomitalia.it>
2022-07-19 21:38:25 +0200

    fixed google analytics

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import ReactGA from 'react-ga';
 import './App.css';
 import InputForm from './components/InputForm';
 
-const TRACKING_ID = "G-G6CNVXNVTF"
-ReactGA.initialize(TRACKING_ID);
-ReactGA.pageview(window.location.pathname + window.location.search);
+
 
 function App() {
+
+  const TRACKING_ID = "G-G6CNVXNVTF"
+  ReactGA.initialize(TRACKING_ID);
+  ReactGA.pageview(window.location.pathname + window.location.search);  
   return (
     <div className="App">
       <InputForm/>
